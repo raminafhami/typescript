338367d02eeb45ef1fb581d843a85454c22467ca
raminafhami <afhami.ramin@yahoo.com>
2022-06-04 19:53:32 +0430

    js in ts

---
+++ compile/sandbox.js
@@ -0,0 +1,5 @@
+var firstName = "Ramin";
+console.log(firstName);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (x) { return console.log(x); });
---
+++ compile/sandbox.ts
@@ -0,0 +1,9 @@
+const firstName = "Ramin";
+console.log(firstName);
+
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+
+inputs.forEach(x => console.log(x));
+
+
